6c12043eb92653a95af0cc0573f371a584d1c826
Nenad Andonov <nenad.andonov@yahoo.com>
2024-11-03 19:32:40 +0000

    Create Contact Us page and send email option

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    user_name = 'nenad.rosoman@gmail.com'
+    password = 'bbxqirltgzbaxghn'
+    context = ssl.create_default_context()
+    receiver = 'nenad.andonov@yahoo.com'
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
